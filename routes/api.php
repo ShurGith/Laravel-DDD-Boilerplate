bf86e63d977bdfb5f78cd59e7447a5edc8b0ee3e
ShurGith <75266693+ShurGith@users.noreply.github.com>
2025-03-26 21:30:44 +0100

    ads

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,12 @@
 <?php
-    
-    use Illuminate\Http\Request;
-    use Illuminate\Support\Facades\Route;
-    
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    })->middleware('auth:sanctum');
-    
-    Route::prefix('platform_purchase')->group(base_path('src/platform/purchase/infrastructure/routes/api.php'));
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::prefix('admin_user')->group(base_path('src/admin/user/infrastructure/routes/api.php'));
+
+Route::prefix('admin_counter')->group(base_path('src/admin/counter/infrastructure/routes/api.php'));
